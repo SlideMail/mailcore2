09bf770c15a552e03a04da9431349e83b8e1bade
Vu Tran <vu0tran@gmail.com>
2015-04-20 11:25:04 -0700

    ok change build path location

--- scripts/build-mailcore2-osx.sh
+++ scripts/build-mailcore2-osx.sh
@@ -6,8 +6,8 @@ popd > /dev/null
 
 . "$scriptpath/include.sh/build-dep.sh"
 
-url="https://github.com/mailcore/mailcore2.git"
-rev=2f6f1ca69d59e2692f85d56941e7d996195cd6e8
+url="https://github.com/SlideMail/mailcore2.git"
+rev=426439ca195ea66f303af74c26acf1fa7032e58f
 name="mailcore2-osx"
 xcode_target="static mailcore2 osx"
 xcode_project="mailcore2.xcodeproj"
